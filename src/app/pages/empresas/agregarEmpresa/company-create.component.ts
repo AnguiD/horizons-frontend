4577e941d586a5afbba58a80c0337d83093631f6
Angelica Guerrero <angelica.guerrero@badak.mx>
2025-06-17 15:03:03 -0600

    feat: Cambio en modal de nueva empresa

--- src/app/pages/empresas/agregarEmpresa/company-create.component.ts
+++ src/app/pages/empresas/agregarEmpresa/company-create.component.ts
@@ -1,40 +1,26 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SidebarComponent } from '../../sidebar/sidebar.component';
-import { HeaderComponent } from "../../header/header.component";
-
-interface CompanyPayload {
-  nombre: string;
-  direccion: string;
-  telefono: string;
-  email: string;
-}
 
 @Component({
-  selector: 'app-company-create',
+  selector: 'company-create',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './company-create.component.html',
   styleUrls: ['./company-create.component.css']
 })
 export class CompanyCreateComponent {
-  empresa: CompanyPayload = {
-    nombre: 'Ejemplo S.A.',
-    direccion: 'Av. Demo 100',
-    telefono: '55-0000-1111',
-    email: 'contacto@ejemplo.com'
+  @Output() cerrar = new EventEmitter<void>();
+
+  newEmpresa = {
+    nombre: '',
+    direccion: '',
+    telefono: '',
+    email: ''
   };
 
-  constructor(private router: Router) {}
-
-  save() {
-    console.log('Creando empresa:', this.empresa);
-    this.router.navigate(['/empresas']);
-  }
-
-  cancel() {
-    this.router.navigate(['/empresas']);
+  guardar() {
+    console.log('Nueva Empresa Registrada:', this.newEmpresa);
+    this.cerrar.emit();
   }
 }
